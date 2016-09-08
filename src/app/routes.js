547417ca75e8f52a536d5bd7601cc7c63272547a
Ivan Matiishyn <matiyishyn.ivan@gmail.com>
2016-09-08 14:33:55 +0200

    UPDATED ui-router to use components

--- src/app/routes.js
+++ src/app/routes.js
@@ -1,26 +1,29 @@
 export default routesConfig;
 
+// Using latest UI-ROUTER
+// docs here: https://ui-router.github.io/docs/latest/
+
 /** @ngInject */
 function routesConfig($stateProvider, $urlRouterProvider, $locationProvider) {
-    $locationProvider.html5Mode(true).hashPrefix('!');
-    $urlRouterProvider.otherwise('/');
+  $locationProvider.html5Mode(true).hashPrefix('!');
+  $urlRouterProvider.otherwise('/');
 
-    $stateProvider
-        .state('app', {
-            url: '/',
-            template: `<app></app>`
-        })
-        .state('app.about', {
-            url: 'about',
-            template: '<about></about>'
-        })
-        .state('app.dashboard', {
-            url: 'dashboard',
-            template: '<dashboard></dashboard>'
-        })
-        .state('app.techs', {
-            url: 'techs',
-            template: '<fountain-techs></fountain-techs>'
-        })
+  $stateProvider
+    .state('app', {
+      url: '/',
+      component: 'app'
+    })
+    .state('app.about', {
+      url: 'about',
+      component: 'about'
+    })
+    .state('app.dashboard', {
+      url: 'dashboard',
+      component: 'dashboard'
+    })
+    .state('app.techs', {
+      url: 'techs',
+      component: 'fountainTechs'
+    })
 
 }
